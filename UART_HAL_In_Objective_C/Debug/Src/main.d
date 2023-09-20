93dbae8eab23a8548df1025c3ca8d551b4257b96
Muhammed-Ubaid <59333976+Muhammed-Ubaid@users.noreply.github.com>
2023-09-20 22:14:21 +0530

    UART HAL project in Objective C
    Developed UART HAL in Objective C

---
+++ UART_HAL_In_Objective_C/Debug/Src/main.d
@@ -0,0 +1,5 @@
+Src/main.o: ../Src/main.c ../Inc/my_stm32f4_uart_lib.h \
+ ../Inc/my_stm32f4_uart_driver.h ../Inc/my_stm32f4_gpio_driver.h
+../Inc/my_stm32f4_uart_lib.h:
+../Inc/my_stm32f4_uart_driver.h:
+../Inc/my_stm32f4_gpio_driver.h:
---
+++ UART_HAL_In_Objective_C/Debug/Src/my_stm32f4_uart_lib.d
@@ -0,0 +1,6 @@
+Src/my_stm32f4_uart_lib.o: ../Src/my_stm32f4_uart_lib.c \
+ ../Inc/my_stm32f4_uart_lib.h ../Inc/my_stm32f4_uart_driver.h \
+ ../Inc/my_stm32f4_gpio_driver.h
+../Inc/my_stm32f4_uart_lib.h:
+../Inc/my_stm32f4_uart_driver.h:
+../Inc/my_stm32f4_gpio_driver.h:
